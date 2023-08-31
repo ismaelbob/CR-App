f309241c7c3a0282d27973dacdede2311cb19089
bob <bobismaeljg@gmail.com>
2023-08-31 10:47:09 -0400

    Commit inicial, barra de menu terminado

---
+++ home/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/general.css">
+    <link rel="stylesheet" href="../css/home.css">
+    <link rel="stylesheet" href="../libraries/bootstrap/css/bootstrap.min.css">
+    <title>Inicio | RKT App</title>
+</head>
+<body>
+    <div class="custom-container">
+        <div class="main container">
+            <div class="main_order"></div>
+            <div class="main_list"></div>
+            <div class="main_summary"></div>
+        </div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary position-fixed bottom-0 w-100 custom-shadow">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="./">Rooster King Trafalgar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a class="nav-link" href="./">Inicio</a></li>
+                        <li class="nav-item"><a class="nav-link" href="../administrar/">Administrar</a></li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a class="nav-link" href="../usuario/">Usuario</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</body>
+<script src="../libraries/bootstrap/js/bootstrap.min.js"></script>
+</html>     
